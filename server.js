fa4fdf2f7b82b1b1b960da8c7b26a5d8c4eec2c7
limjens <limjens49@gmail.com>
2026-04-20 16:09:00 +0800

    fixed routes

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ app.use(express.json());
 
 app.use("/api/persons", require("./routes/persons"));
 app.use("/api/patients", require("./routes/patients"));
-//app.use("/api/appointment", require("./routes/appointment"));
+app.use("/api/appointment", require("./routes/appointment"));
 app.use("/api/burials", require("./routes/burials"));
-//app.use("/api/dentists", require("./routes/dentists"));
+app.use("/api/dentists", require("./routes/dentists"));
 
 app.listen(3000, () => {
   console.log("Server is running baby.");
