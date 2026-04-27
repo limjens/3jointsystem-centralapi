986befdcfe249946c96a6e3bfb1ce645750510e6
limjens <limjens49@gmail.com>
2026-04-27 16:35:27 +0800

    Added cors

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+const cors = require("cors");
 const express = require("express");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", require("./routes/users"));
